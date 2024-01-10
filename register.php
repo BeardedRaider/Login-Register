3cdb1346c8a968cc236038ca026a346b7ac4f78b
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-01-10 18:49:30 +0000

    Update style.css and register.php

--- register.php
+++ register.php
@@ -5,16 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>register</title>
+    <title>register</title>    
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-    <!-- custom css file link -->
-    <link rel="stylesheet" href="css/style.css">
+
 
 <div class="form-container">
     <form action="" method="post" enctype="multipart/form-data">
+        <h3>register now</h3>
+        
         <input type="text" name="name" placeholder="enter username" class="box" required>
+        <input type="email" name="email" placeholder="enter email" class="box" required>
+        <input type="password" name="password" placeholder="enter password" class="box" required>
+        <input type="password" name="cpassword" placeholder="confirm password" class="box" required>
+        
+        <input type="file" class="box" accept="image/jpg, image/jpeg, image/png">
+        <input type="submit" value="register now" class="btn">
+
+        <p>already have an account? <a href="login.php">login now</a></p>
     </form>
 </div>
 
