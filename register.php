637555c4ae679b590dcc94b373a4da188df76505
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-01-10 18:27:04 +0000

    Add custom CSS file link and form in register.php

--- register.php
+++ register.php
@@ -5,9 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>register</title>
 </head>
 <body>
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+<div class="form-container">
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="enter username" class="box" required>
+    </form>
+</div>
 
 
 
