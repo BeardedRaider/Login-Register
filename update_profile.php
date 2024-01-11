2e46ad50cc7e3b87da75c176ebf120efe635dc19
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-01-11 14:45:21 +0000

    Add styles for profile page and update profile functionality  COMPLETE

--- update_profile.php
+++ update_profile.php
@@ -4,6 +4,47 @@ include 'config.php';
 session_start();
 $user_id = $_SESSION['user_id'];
 
+if(isset($_POST['update_profile'])){
+
+    $update_name = mysqli_real_escape_string($conn, $_POST['update_name']);
+    $update_email = mysqli_real_escape_string($conn, $_POST['update_email']);
+    
+    mysqli_query($conn, "UPDATE `user_form` SET name = '$update_name', email = '$update_email' WHERE id = '$user_id'" ) or die('query failed');
+
+    $old_pass = $_POST['old_pass'];
+    $update_pass = mysqli_real_escape_string($conn, md5($_POST['update_pass']));
+    $new_pass = mysqli_real_escape_string($conn, md5($_POST['new_pass']));
+    $confirm_pass = mysqli_real_escape_string($conn, md5($_POST['confirm_pass']));
+
+    if(!empty('$update_pass') || !empty('$new_pass') || !empty('$confirm_pass')) {
+        if($update_pass != $old_pass){
+            $message[] = 'previous password does not match!';
+        }elseif($new_pass != $confirm_pass){
+            $message[] = 'new password does not match!';
+        }else{
+            mysqli_query($conn, "UPDATE `user_form` SET password = '$confirm_pass' WHERE id = '$user_id'" ) or die('query failed');
+            $message[] = 'password updated successfully';
+
+        }
+    $update_image = $_FILES['update_image']['name'];
+    $update_image_size = $_FILES['update_image']['size'];
+    $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+    $update_image_folder = 'uploaded_image/'.$update_image;
+
+    if(!empty('$update_image')){
+        if($update_image_size > 2000000){
+            $message[] = 'image size too large!';
+        }else{
+            $image_update_query = mysqli_query($conn, "UPDATE `user_form` SET image = '$update_image' WHERE id = '$user_id'" ) or die('query failed');
+            if($image_update_query){
+                move_uploaded_file($update_image_tmp_name, $update_image_folder);
+            }
+            $message[] = 'image updated successfully';
+            }
+        }
+    }
+};
+
 ?>
 
 <!DOCTYPE html>
@@ -26,17 +67,27 @@ $user_id = $_SESSION['user_id'];
             if(mysqli_num_rows($select) > 0){
                 $fetch = mysqli_fetch_assoc($select);   
             }
-            if($fetch['image'] == ''){
-                echo '<img src="images/default-user.png">';
-            }else{
-                echo '<img src="uploaded_image/'.$fetch['image'].'">';
-
-            }
+            
         ?>
 
         <form action="" method="post" enctype="multipart/form-data">
+            <?php
+            
+                if($fetch['image'] == ''){
+                    echo '<img src="images/default-user.png">';
+                }else{
+                    echo '<img src="uploaded_image/'.$fetch['image'].'">';
+
+                }
+                if(isset($message)){
+                    foreach($message as $message){
+                        echo '<div class="message">'.$message.'</div>';
+                    }
+                }
+            
+            ?>
             <div class="flex">
-                <div class="inputbox">
+                <div class="inputBox">
                     <span>username :</span>
                     <input type="text" name="update_name" value="<?php echo $fetch['name'] ?>" class="box">
                     <span>your email :</span>
@@ -45,7 +96,7 @@ $user_id = $_SESSION['user_id'];
                     <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
                 </div>
                 <div class="inputBox">
-                    <input type="hidden" name="old_name" value="<?php echo $fetch['password'] ?>">
+                    <input type="hidden" name="old_pass" value="<?php echo $fetch['password'] ?>">
                     <span>previous password :</span>
                     <input type="password" name="update_pass" placeholder="enter previous password" class="box">
 
